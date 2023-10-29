41735a76fe2304e5846134e14f516ec2e07263fa
AndriiKozlovskyi <andrii@advasoftware.com>
2023-10-29 16:22:29 +0100

    api

--- src/api/exchange_api.ts
+++ src/api/exchange_api.ts
@@ -1,7 +1,15 @@
+import api_factory from "./api_factory";
+import BaseApiResponse from "./response/base_api_response";
+
 export default class ExhangeApi {
 
-  base_url = "http://localhost:8080";
+  baseUrl = "http://localhost:8080";
 
-  
-  
+  async getOrderBooks(exchange: string, symbols: string[]): Promise<BaseApiResponse<string>> {
+    return await api_factory.getInstance().post(`${this.baseUrl}/${exchange}/order_books`, symbols);
+  }
+
+  async getTradingPairs(exchange: string): Promise<BaseApiResponse<string>> {
+    return await api_factory.getInstance().get(`${this.baseUrl}/${exchange}/trading_pairs`);
+  }
 }
